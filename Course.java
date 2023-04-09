bddff304686432df30ec7c4b8bf58790c86e7e0c
Elina Boichuk <elinapostbox@gmail.com>
2023-04-09 23:49:35 +0300

    secondCommit

---
+++ Course.java
@@ -0,0 +1,7 @@
+public class Course {
+    public int a = 40;
+
+    public void print(String a) {
+        System.out.println(a);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        int a = 15;
+        Course Meaning = new Course();
+        String.format("---<%d>---", a);
+    }
+}
